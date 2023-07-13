d9c8d97ced2ada9a45fef729762907a182b4fbf8
pratiksj <pratiksharai728@gmail.com>
2023-07-13 22:40:39 +0545

    feat: complete 1.4 in which placed object into the array

--- exercises/part-1/courseinfo/src/components/Content.js
+++ exercises/part-1/courseinfo/src/components/Content.js
@@ -3,9 +3,9 @@ import Part from "./Part";
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.part1} exercise={props.exercises1} />
-      <Part part={props.part2} exercise={props.exercises2} />
-      <Part part={props.part3} exercise={props.exercises3} />
+      <Part part={props.parts[0].name} exercise={props.parts[0].exercises} />
+      <Part part={props.parts[1].name} exercise={props.parts[1].exercises} />
+      <Part part={props.parts[2].name} exercise={props.parts[2].exercises} />
     </div>
   );
 };
