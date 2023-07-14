94d7545c0ecaab0057a4f1721c648cba2dc25532
pratiksj <pratiksharai728@gmail.com>
2023-07-14 17:44:35 +0545

    feat: complete 1:7

--- exercises/part-1/unicafe/src/App.js
+++ exercises/part-1/unicafe/src/App.js
@@ -1,27 +1,36 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  const goodHandler =()=>{
-   setGood(good+1)
-  }
+  const goodHandler = () => {
+    setGood(good + 1);
+  };
+  const neutralHandler = () => {
+    setNeutral(neutral + 1);
+  };
+  const badHandler = () => {
+    setBad(bad + 1);
+  };
 
   return (
     <div>
       <h1>give feedback</h1>
       <button onClick={goodHandler}>good</button>
-      <button>neutral</button>
-      <button>bad</button>
+      <button onClick={neutralHandler}>neutral</button>
+      <button onClick={badHandler}>bad</button>
       <h1>statistics</h1>
       <p>good:{good}</p>
       <p>neutral:{neutral}</p>
       <p>bad:{bad}</p>
+      <p>all:{good + neutral + bad}</p>
+      <p>average:{(good + neutral + bad) / 3}</p>
+      <p>positive:{(good / (good + neutral + bad)) * 100}%</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
