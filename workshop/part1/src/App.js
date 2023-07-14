8ce73889fff2a5498f7fff5ce2964230bb5c8f97
pratiksj <pratiksharai728@gmail.com>
2023-07-14 14:34:24 +0545

    feat: add event handler

--- workshop/part1/src/App.js
+++ workshop/part1/src/App.js
@@ -1,18 +1,21 @@
 import { useState } from "react"
 import Mytag from "./compoenents/Mytag"
+import MyButton from "./compoenents/MyButton"
 
 
 const App = () => {
 const myState = useState(1)
 const myCount =myState[0]
 const setMycount = myState[1]
-setTimeout(()=>{
+
+const increaseCount=()=>{
   setMycount(myCount+1)
-},1000)
+}
 
   let name1 = 'Amir'
   return ( <div>
     <h3>{myCount}</h3>
+    <MyButton buttonHandler={increaseCount}/>
     <Mytag name ={name1} lastName= "someLast Name1"/>
     <Mytag name ="Niru" lastName= "someLast Name2"/>
     <Mytag name ="Ashim" lastName= "someLast Name3"/>
---
+++ workshop/part1/src/compoenents/MyButton.js
@@ -0,0 +1,9 @@
+
+
+const MyButton = (props) => {
+  return (
+    <button onClick={props.buttonHandler}>click me</button>
+  )
+}
+
+export default MyButton
