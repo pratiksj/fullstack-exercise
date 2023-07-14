b10833d02df6f69183520dd494d32718fbb73922
pratiksj <pratiksharai728@gmail.com>
2023-07-14 18:09:51 +0545

    feat:complete 1.9

--- exercises/part-1/unicafe/src/components/Statistics.js
+++ exercises/part-1/unicafe/src/components/Statistics.js
@@ -1,15 +1,23 @@
 const Statistics = ({ good, neutral, bad }) => {
-  return (
-    <div>
-      <h1>statistics</h1>
-      <p>good:{good}</p>
-      <p>neutral:{neutral}</p>
-      <p>bad:{bad}</p>
-      <p>all:{good + neutral + bad}</p>
-      <p>average:{(good - bad) / 3}</p>
-      <p>positive:{(good / (good + neutral + bad)) * 100}%</p>
-    </div>
-  );
+  const percentage = () => {
+    let total = good + neutral + bad;
+    return (good / total) * 100;
+  };
+  if (good !== 0 || neutral !== 0 || bad !== 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>good:{good}</p>
+        <p>neutral:{neutral}</p>
+        <p>bad:{bad}</p>
+        <p>all:{good + neutral + bad}</p>
+        <p>average:{(good - bad) / 3}</p>
+        <p>positive:{percentage()}%</p>
+      </div>
+    );
+  } else {
+    return <h1>No feedback given</h1>;
+  }
 };
 
 export default Statistics;
