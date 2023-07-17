5307ef70ffed387ccc814d8d862b44a052b62f89
pratiksj <pratiksharai728@gmail.com>
2023-07-17 22:51:27 +0545

    feat: complete 2.6 exercise

---
+++ exercises/part2/src/App.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    const newObject = {
+      name: newName,
+    };
+    setPersons([...persons, newObject]);
+    console.log(persons, "state");
+
+    setNewName("");
+  };
+
+  const handleOnChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleOnChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person, index) => {
+        return <div key={index}> {person.name}</div>;
+      })}
+    </div>
+  );
+};
+
+export default App;
