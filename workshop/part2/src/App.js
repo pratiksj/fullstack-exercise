4cff7175472b73dd3ce50204c9df60fdeac93929
pratiksj <pratiksharai728@gmail.com>
2023-07-17 22:20:11 +0545

    feat: filter the notes which are true

--- workshop/part2/src/App.js
+++ workshop/part2/src/App.js
@@ -3,15 +3,48 @@ import Note from "./components/Note";
 
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("new note..");
+  const [showAll, setShowAll] = useState(true);
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      data: new Date().toISOString(),
+      important: Math.random() < 0.5 ? true : false,
+      id: notes.length + 1,
+    };
+    setNotes(notes.concat(noteObject));
+
+    setNewNote("");
+  };
+  const handleOnChange = (event) => {
+    setNewNote(event.target.value);
+  };
+
+  const notesToshow = showAll
+    ? notes
+    : notes.filter((note) => note.important === true);
 
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
+        </button>
+      </div>
       <ul>
-        {notes.map((note) => (
-          <Note key={note.id} note={note} />
+        {notesToshow.map((note) => (
+          <li key={note.id}>
+            <Note key={note.id} note={note} />
+          </li>
         ))}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleOnChange} />
+        <button>save</button>
+      </form>
     </div>
   );
 };
