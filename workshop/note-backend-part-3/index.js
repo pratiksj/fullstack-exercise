5822a49b423396aedb680ca74d165ad86c2d18dd
pratiksj <pratiksharai728@gmail.com>
2023-07-31 22:32:45 +0545

    feat:complete 3.1 and 3.2

--- workshop/note-backend-part-3/index.js
+++ workshop/note-backend-part-3/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 // var removeDuplicates = function (nums) {
 //   let index = 0;
 
@@ -48,7 +49,33 @@ app.get("/api/notes", (req, res) => {
   res.json(notes);
 });
 
-const PORT = 3002;
+app.get("/api/notes/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const note = notes.find((note) => note.id === id);
+  if (note) {
+    res.json(note);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  console.log(typeof id, "from id");
+  notes = notes.filter((note) => note.id !== id);
+  console.log(notes, "from code");
+
+  response.status(204).end();
+});
+
+app.post("/api/notes", (request, response) => {
+  const note = request.body;
+  note.id = notes.length + 1;
+  notes.push(note);
+  response.status(201).json(note);
+});
+
+const PORT = 3003;
 app.listen(PORT, () => {
   console.log(`Server listening on the port${PORT}`);
 });
