2e0aaf71f3566e84d62b3fe2de9f897c1d28600b
pratiksj <pratiksharai728@gmail.com>
2023-08-01 09:12:11 +0545

    feat:complete 3.4 exercise of part-3

--- exercises/part-3/index.js
+++ exercises/part-3/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 
-const persons = [
+app.use(express.json())
+
+let persons = [
   {
     id: 1,
     name: "Arto Hellas",
@@ -34,14 +36,22 @@ app.get("/info", (request, response) => {
   );
 });
 
-app.get('/api/persons/:id',(request,response)=>{
+app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  const person = persons.find(person=>person.id===id)
-  if(person){
+  const person = persons.find(person => person.id === id)
+  if (person) {
     response.json(person)
-  }else{
+  } else {
     response.status(404).end()
   }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  console.log(typeof id, 'they')
+  persons = persons.filter((person) => person.id !== id)
+
+  response.status(204).end()
 })
 
 const PORT = 3004;
