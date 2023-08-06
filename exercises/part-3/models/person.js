ccf600d376f33c4f57d94fdd134c0a37912838db
pratiksj <pratiksharai728@gmail.com>
2023-08-06 12:32:47 +0545

    feat:add custom validator in 3.20

--- exercises/part-3/models/person.js
+++ exercises/part-3/models/person.js
@@ -23,12 +23,18 @@ const personSchema = new mongoose.Schema({
 
     },
     number: {
-        type: Number,
-        minLength: 3,
-        required: true
+        type: String,
+        validate: {
+            validator: function (v) {
+                return /\d{2,3}-\d{7,}/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },
+
     }
-    // name: String,
-    // number: String,
+
+
+
 })
 
 personSchema.set('toJSON', {
