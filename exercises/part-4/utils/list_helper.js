136545cfe2cc0362282cd65bf6baba27559a4d54
pratiksj <pratiksharai728@gmail.com>
2023-08-09 17:17:41 +0545

    feat: complete 4.6 and 4.7   using lodash  library

--- exercises/part-4/utils/list_helper.js
+++ exercises/part-4/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
     let value = 1
     return value
@@ -17,8 +19,46 @@ const mostLikes = (blogs) => {
     return mostLi
 }
 
+const mostBlog = (blogs) => {
+    const groupedbyAuthor = _.groupBy(blogs, 'author')
+    const authorWithBlogCount = _.map(groupedbyAuthor, (blogs, author) => ({
+        author,
+        blogs: blogs.length
+    }))
+    //console.log(authorWithBlogCount, 'testingcount')
+    const authorWithBlog = _.maxBy(authorWithBlogCount, 'blogs')
+    //console.log(authorWithBlog, 'last outcome')
+    return authorWithBlog
+
+
+}
+
+const massLikes = (blogs) => {
+    const groupByAuthorName = _.groupBy(blogs, 'author')
+    //console.log(groupByAuthorName, 'first')
+    const authorWithLikes = _.map(groupByAuthorName, (blogs, author) => ({
+        author,
+        likes: _.map(blogs, 'likes')
+    }))
+    const sumOfLikes = _.map(authorWithLikes, (author) => (
+        {
+            author: author.author,
+            likes: _.sumBy(author.likes)
+        }
+    )
+
+    )
+    //console.log(sumOfLikes, 'she')
+    const authorWithBlog = _.maxBy(sumOfLikes, 'likes')
+    //console.log(authorWithBlog, 'sim')
+    return authorWithBlog
+
+}
+
 module.exports = {
     dummy,
     totalLikes,
-    mostLikes
+    mostLikes,
+    mostBlog,
+    massLikes
 }
