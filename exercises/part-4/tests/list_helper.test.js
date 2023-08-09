10cfc041adbcba1a90ce0949c4476bcd595603e3
pratiksj <pratiksharai728@gmail.com>
2023-08-09 11:01:48 +0545

    feat: complete 4:5 exercise of part4

--- exercises/part-4/tests/list_helper.test.js
+++ exercises/part-4/tests/list_helper.test.js
@@ -25,3 +25,30 @@ describe('total likes', () => {
         expect(result).toBe(5)
     })
 })
+
+
+describe('most likes', () => {
+    const listWithOneBlog = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: "5a422b3a1b54a676234d17f9",
+            title: "Canonical string reduction",
+            author: "Edsger W. Dijkstra",
+            url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+            likes: 12,
+            __v: 0
+        },
+    ]
+
+    test('when list has only one blog, equals the likes of that', () => {
+        const result = listHelper.mostLikes(listWithOneBlog)
+        expect(result).toBe(12)
+    })
+})
--- exercises/part-4/utils/list_helper.js
+++ exercises/part-4/utils/list_helper.js
@@ -9,8 +9,16 @@ const totalLikes = (blogs) => {
     let total = blogs.reduce((accumulator, currentVal) => currentVal.likes + accumulator, 0)
     return total
 }
+const mostLikes = (blogs) => {
+    let mostLi = blogs.reduce((maxItem, currentItem) => {
+        return currentItem.likes > maxItem ? currentItem.likes : maxItem
+    }, 0)
+
+    return mostLi
+}
 
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    mostLikes
 }
