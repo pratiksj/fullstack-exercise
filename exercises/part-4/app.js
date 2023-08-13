a367e2eaedd6edc8ac64893b334204d91814efef
pratiksj <pratiksharai728@gmail.com>
2023-08-13 13:19:00 +0545

    feat:complete 4.18 of part4-d

--- exercises/part-4/app.js
+++ exercises/part-4/app.js
@@ -5,6 +5,7 @@ const app = express()
 const blogRouter = require('./controllers/blog')
 //const middleware = require('./utils/middleware')
 const usersRouter = require('./controllers/user')
+const loginRouter = require('./controllers/login')
 
 const logger = require('./utils/logger')
 
@@ -13,6 +14,7 @@ app.use(express.json())
 //app.use(middleware.requestLogger)
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 //app.use(middleware.unknownEndpoint)
 //app.use(middleware.errorHandler)
 
