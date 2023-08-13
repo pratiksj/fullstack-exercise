3f848590aa906a682001f70cf2afdc751febf59d
pratiksj <pratiksharai728@gmail.com>
2023-08-13 10:05:54 +0545

    feat: workshop of part 4-c

---
+++ workshop/note-backend-part-3/tests/note_api.test.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const Note = require('../models/note')
+const helper = require('./test_helper')
+
+
+beforeEach(async () => {
+    await Note.deleteMany({})
+    let noteObject = new Note(helper.initialNotes[0])
+    await noteObject.save()
+    noteObject = new Note(helper.initialNotes[1])
+    await noteObject.save()
+})
+
+
+test('notes are returned as json', async () => {
+    await api
+        .get('/api/notes')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+}, 100000)
+
+test('all note are returned as json', async () => {
+    const allNote = helper.notesInDb()
+    console.log(allNote, 'test')
+
+})
+
+afterAll(async () => {
+    await mongoose.connection.close()
+})
