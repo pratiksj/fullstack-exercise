f9f34c84494b3f7d1f981e5127a956d9eb973f10
pratiksj <pratiksharai728@gmail.com>
2023-08-21 15:59:06 +0545

    feat:add eslint configuration

--- workshop/part2/src/components/Togglable.js
+++ workshop/part2/src/components/Togglable.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { useState, forwardRef, useImperativeHandle } from 'react'
 
 export const Togglable = forwardRef((props, refs) => {
@@ -29,3 +30,7 @@ export const Togglable = forwardRef((props, refs) => {
     )
 }
 )
+
+Togglable.propTypes = {
+    buttonLabel: PropTypes.string.isRequired
+}
