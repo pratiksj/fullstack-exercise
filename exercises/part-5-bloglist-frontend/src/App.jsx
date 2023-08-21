5a3041614df8dca6bd85ad37f1457fd952055627
pratiksj <pratiksharai728@gmail.com>
2023-08-21 11:10:37 +0545

    feat:compete 5.2 of part-5a

--- exercises/part-5-bloglist-frontend/src/App.jsx
+++ exercises/part-5-bloglist-frontend/src/App.jsx
@@ -14,13 +14,22 @@ const App = () => {
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
-    )  
+    ) 
+  let loggedInUser = window.localStorage.getItem('loggedInUser')
+  if(loggedInUser){
+    let user = JSON.parse(loggedInUser)
+   setUser(user)
+  } 
   }, [])
+
+
+
 
   const handleLogin= async(event)=>{
    event.preventDefault()
    try{
 const user = await loginService.login({username,password})
+window.localStorage.setItem('loggedInUser',JSON.stringify(user))
 setUser(user)
 setUsername('')
 setPassword('')
@@ -46,10 +55,16 @@ setPassword('')
     )
   }
 
+  const logOut=()=>{
+  window.localStorage.removeItem('loggedInUser')
+  setUser(null)
+  
+  }
+
   return (
     <div>
       <h2>blogs</h2>
-      {user===null?<div>log into application{loginForm()} </div>:<div>{user.name} has logged in {blogs.map(blog =>
+      {user===null?<div>log into application{loginForm()} </div>:<div>{user.name} has logged in<button onClick={logOut}>logOut</button> {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}</div>}
       
--- exercises/part-5-bloglist-frontend/src/services/blogs.js
+++ exercises/part-5-bloglist-frontend/src/services/blogs.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
+
+
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
