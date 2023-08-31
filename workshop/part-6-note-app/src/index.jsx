3ce5c264797c56161f65b546749eae4ea62041c6
pratiksj <pratiksharai728@gmail.com>
2023-08-31 15:46:32 +0545

    feat:shift store related code to dedicated file for store

--- workshop/part-6-note-app/src/index.jsx
+++ workshop/part-6-note-app/src/index.jsx
@@ -1,18 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//import { createStore, combineReducers } from "redux";
-//toolkit replacement of createStore and combineReducers
-
 import { Provider } from "react-redux";
 
 import App from "./App";
-import noteReducer from "./reducers/noteReducer";
-import filterReducer from "./reducers/filterReducer";
-import { configureStore } from "@reduxjs/toolkit";
-const store = configureStore({
-  reducer: { notes: noteReducer, filter: filterReducer },
-});
+import store from "./store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
