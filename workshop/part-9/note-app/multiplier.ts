9edb8f4d9150f7009072a1b43c307b7e22c1ee37
pratiksj <pratiksharai728@gmail.com>
2023-10-04 15:08:20 +0545

    feat:allow return type to string value

--- workshop/part-9/note-app/multiplier.ts
+++ workshop/part-9/note-app/multiplier.ts
@@ -1,12 +1,12 @@
 type Operation= 'multiply'|'add'|'divide'
 
-const multiplicator = (a:number, b:number, op:Operation) => {
+const multiplicator = (a:number, b:number, op:Operation):number|string => {
     if (op === 'multiply') {
         return a * b;
       } else if (op === 'add') {
         return a + b;
       } else if (op === 'divide') {
-        if (b === 0) return 'can\'t divide by 0!';
+        if (b === 0) return 'this cannot be done';
         return a / b;
       }
   }
